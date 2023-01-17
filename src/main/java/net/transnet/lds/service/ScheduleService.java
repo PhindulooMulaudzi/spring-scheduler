f8cda6d35e096742ac2105b3c564d675d60949f8
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-01-17 11:51:53 +0200

    added fileWatcher
    we have taken advantage of schedule tasks to create a directory file watcher

--- src/main/java/net/transnet/lds/service/ScheduleService.java
+++ src/main/java/net/transnet/lds/service/ScheduleService.java
@@ -1,5 +1,16 @@
 package net.transnet.lds.service;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -12,9 +23,52 @@ import org.springframework.stereotype.Service;
  */
 @Component
 public class ScheduleService {
-	//should print hello every 5000ms or 5s
+	// should print hello every 5000ms or 5s
 	@Scheduled(fixedRate = 5000)
 	public void printHello() {
 		System.out.println("Hello, World!");
+	}
+
+	@Scheduled(fixedRate = 2000)
+	public void watchTask() {
+		try {
+			WatchService watcher = FileSystems.getDefault().newWatchService();
+			Path dir = Paths.get("C:\\Users\\OWen\\Documents\\workspace-spring-tool-suite\\locomotive-file-requestor\\downloads");
+			dir.register(watcher, ENTRY_CREATE);
+			System.out.println("Watch Service registered for dir: " + dir.getFileName());
+
+			while (true) {
+				WatchKey key;
+				try {
+					key = watcher.take();
+				} catch (InterruptedException ex) {
+					return;
+				}
+
+				for (WatchEvent<?> event : key.pollEvents()) {
+					WatchEvent.Kind<?> kind = event.kind();
+
+					@SuppressWarnings("unchecked")
+					WatchEvent<Path> ev = (WatchEvent<Path>) event;
+					Path fileName = ev.context();
+					
+					if (kind == ENTRY_CREATE) {
+						System.out.println(kind.name() + ": " + fileName);
+						// Change status to started download
+					}
+					
+					if (kind == ENTRY_MODIFY) {
+						System.out.println(kind.name() + ": " + fileName);
+						// Change status to download in progress if not already marked for in progress
+					}
+				}
+				boolean valid = key.reset();
+				if (!valid) {
+					break;
+				}
+			}
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 }
